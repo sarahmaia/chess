a882ccdfa3a32e9ecad7d5ab1adbff89832d7118
sarahcrismaia@gmail.com <sarahmaia21799@gmail.com>
2024-07-22 22:31:35 -0600

    DAOs potentially done

--- server/src/main/java/dataaccess/DAO/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/DAO/MemoryAuthDAO.java
@@ -1,4 +1,27 @@
 package dataaccess.DAO;
 
+import model.AuthData;
+import model.UserData;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MemoryAuthDAO {
+    private final Map<String, AuthData> auths = new HashMap<>();
+
+    public MemoryAuthDAO() {}
+
+    public AuthData createAuth(String username) {
+        AuthData auth = new AuthData(AuthData.createToken(), username);
+        auths.put(username, auth);
+        return auth;
+    }
+
+    public AuthData getAuth(String username) {
+        return auths.get(username);
+    }
+
+    public void clear() {
+        auths.clear();
+    }
+
 }
--- server/src/main/java/dataaccess/DAO/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/DAO/MemoryGameDAO.java
@@ -1,4 +1,55 @@
 package dataaccess.DAO;
 
+import model.GameData;
+import chess.ChessGame;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.security.SecureRandom;
+
 public class MemoryGameDAO {
+    private final Map<Integer, GameData> games = new HashMap<>();
+
+    public MemoryGameDAO() {
+    }
+
+    public GameData createGame(String gameName) {
+
+        int newGameID = generateNewGameID();
+
+        GameData newGameData = new GameData(
+                newGameID,
+                null,
+                null, gameName,
+                new ChessGame()
+        );
+
+        games.put(newGameID, newGameData);
+        return newGameData;
+    }
+
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    public Collection<GameData> readAllGames() {
+        return games.values();
+    }
+
+    public void updateGame(GameData game) {
+        games.put(game.gameID(), game);
+    }
+
+    public void clear() {
+        games.clear();
+    }
+
+    private int generateNewGameID() {
+        SecureRandom random = new SecureRandom();
+        int randomID = random.nextInt(Integer.MAX_VALUE);
+        while (games.containsKey(randomID)) {
+            randomID = random.nextInt(Integer.MAX_VALUE);
+        }
+        return randomID;
+    }
 }
