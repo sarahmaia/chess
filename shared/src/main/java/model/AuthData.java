ff74489a5b7cbdf0d2195dc705e0c010d07c32d6
sarahcrismaia@gmail.com <sarahmaia21799@gmail.com>
2024-07-23 23:19:24 -0600

    models done

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,21 +1,26 @@
 package model;
-
-import java.security.SecureRandom;
-import java.util.Base64;
+import java.util.Objects;
+import java.util.UUID;
 
 public record AuthData(String authToken, String username) {
-
-    private static final SecureRandom secureRandom = new SecureRandom();
-    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder().withoutPadding();
-
-    /**
-     * Randomly generates a secure auth token.
-     * This occurs by generating a random string of bytes, then pulling from that for the token
-     * @return String - Random identifier
-     */
     public static String createToken() {
-        byte[] randomBytes = new byte[24];
-        secureRandom.nextBytes(randomBytes);
-        return base64Encoder.encodeToString(randomBytes);
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AuthData authData = (AuthData) o;
+        return Objects.equals(username, authData.username) && Objects.equals(authToken, authData.authToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authToken, username);
     }
 }
--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,5 +1,8 @@
 package model;
+
 import chess.ChessGame;
+
+import java.util.Objects;
 
 public record GameData(
         int gameID,
@@ -9,4 +12,24 @@ public record GameData(
         ChessGame game
 ) {
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameData gameData = (GameData) o;
+        return gameID == gameData.gameID
+                && Objects.equals(game, gameData.game)
+                && Objects.equals(gameName, gameData.gameName)
+                && Objects.equals(whiteUsername, gameData.whiteUsername)
+                && Objects.equals(blackUsername, gameData.blackUsername);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameID, whiteUsername, blackUsername, gameName, game);
+    }
 }
