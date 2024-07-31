313ade323b51caf68f14f6a670942ed98c57a846
sarahcrismaia@gmail.com <sarahmaia21799@gmail.com>
2024-07-31 01:39:51 -0600

    authservice

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,22 +1,23 @@
 package service;
 
-import dataaccess.exceptions.UnauthorizedException;
-import dataaccess.DAO.MemoryAuthDAO;
+import dataaccess.DAO.AuthDAO;
+import dataaccess.exceptions.*;
+import dataaccess.DataAccessException;
 import model.AuthData;
 import model.UserData;
 
 public class AuthService {
-    private final MemoryAuthDAO authDAO;
+    private final AuthDAO authDAO;
 
-    public AuthService(MemoryAuthDAO authDAO) {
+    public AuthService(AuthDAO authDAO) {
         this.authDAO = authDAO;
     }
 
-    public AuthData loginUser(UserData user) throws UnauthorizedException {
+    public AuthData loginUser(UserData user) throws UnauthorizedException, DataAccessException {
         return authDAO.createAuth(user.username());
     }
 
-    public void logoutUser(String authToken) throws UnauthorizedException {
+    public void logoutUser(String authToken) throws UnauthorizedException, DataAccessException {
         AuthData authData = authDAO.getAuthByToken(authToken);
         if (authData == null) {
             throw new UnauthorizedException("unauthorized");
@@ -26,18 +27,18 @@ public class AuthService {
         }
     }
 
-    public AuthData getUserByAuthToken(String authToken) throws UnauthorizedException {
+    public AuthData getUserByAuthToken(String authToken) throws UnauthorizedException, DataAccessException {
         return authDAO.getAuthByToken(authToken);
     }
 
-    public void validateAuthToken(String authToken) throws UnauthorizedException {
+    public void validateAuthToken(String authToken) throws UnauthorizedException, DataAccessException {
         AuthData authData = authDAO.getAuthByToken(authToken);
         if (authData == null || !authData.authToken().equals(authToken)) {
             throw new UnauthorizedException("unauthorized");
         }
     }
 
-    public void clear() {
+    public void clear() throws BadRequestException {
         authDAO.clear();
     }
 }
